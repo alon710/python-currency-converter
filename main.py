c06e8510005f01e52048d7c57b6ace866c9e9c8b
Orbasker <orbasker@gmail.com>
2023-01-09 01:33:37 +0200

    Beutify code.

--- main.py
+++ main.py
@@ -44,4 +44,8 @@ if __name__ == "__main__":
     from dotenv import load_dotenv
 
     load_dotenv()
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=8000,
+    )
