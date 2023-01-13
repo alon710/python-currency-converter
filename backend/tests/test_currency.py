bc5907a3b75356bfaa873c2fb1b309f760646459
Alon Barad <Alon710@gmail.com>
2023-01-14 01:50:20 +0200

    Added tests boylerplate.

---
+++ backend/tests/test_currency.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+from fastapi.testclient import TestClient
+
+from backend.routes.currency import router
+
+client = TestClient(router)
+
+load_dotenv()
+
+
+def test_currency_list_status_code():
+    response = client.get("/currency/list")
+    assert response.status_code == 200
+
+
+def test_specific_currency_value():
+    response = client.get("/currency/", params={"currency_code": "USD"})
+    assert response.status_code == 200
+    assert response.json() == {
+        "symbol": "$",
+        "name": "US Dollar",
+        "symbol_native": "$",
+        "decimal_digits": 2,
+        "rounding": 0,
+        "code": "USD",
+        "name_plural": "US dollars",
+    }
